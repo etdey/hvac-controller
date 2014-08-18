d0604e0011d89cf933004505acbeda34b29eef20
Eric Dey <eric@deys.org>
2014-08-17 19:09:34 -0500

    Adjusted the time correction value based on observed clock skew.

--- timevalues.h
+++ timevalues.h
@@ -19,8 +19,8 @@ extern "C" {
 // This is derived from the TMR0 period, callback rate, and corrected
 // for the observed oscillator error factor.
 // Oscillator error = 1.2231 (> 1.0 is too fast)
-// 100ms / 1.2231 = 81.76ms  ~= 82ms
-#define MSEC_BETWEEN_TIMER_CALLBACKS  82
+// 100ms / 1.2454 = 80.30ms  ~= 80ms
+#define MSEC_BETWEEN_TIMER_CALLBACKS  80
 
 // Times requirements for state transitions. Values are in seconds.
 #define COOL_ON_TO_COOL_OFF         30 * 60
