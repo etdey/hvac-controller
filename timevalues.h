acc41ee1d61488f2f150f97cae4ad628e11aef3f
Eric Dey <eric@deys.org>
2014-08-23 16:20:52 -0500

    Lowered minimum cooling time and raised time between cool cycles.

--- timevalues.h
+++ timevalues.h
@@ -23,16 +23,16 @@ extern "C" {
 #define MSEC_BETWEEN_TIMER_CALLBACKS  82
 
 // Times requirements for state transitions. Values are in seconds.
-#define COOL_ON_TO_COOL_OFF         30 * 60
-#define COOL_OFF_TO_COOL_ON         20 * 60
+#define COOL_ON_TO_COOL_OFF         20 * 60
+#define COOL_OFF_TO_COOL_ON         30 * 60
 #define COOL_OFF_TO_HEAT_ON         60 * 60
 #define HEAT_ON_TO_HEAT_OFF         20 * 60
 #define HEAT_OFF_TO_HEAT_ON         20 * 60
 #define HEAT_OFF_TO_COOL_ON         60 * 60
 #define FAN_BEFORE_COOL_ON          15 * 60
 #define FAN_BEFORE_HEAT_ON          15 * 60
-#define FAN_AFTER_COOL_OFF          15 * 60
-#define FAN_AFTER_HEAT_OFF          15 * 60
+#define FAN_AFTER_COOL_OFF          10 * 60
+#define FAN_AFTER_HEAT_OFF          10 * 60
 #define MAX_COOL_ON                 120 * 60
 #define MAX_HEAT_ON                 180 * 60
 
